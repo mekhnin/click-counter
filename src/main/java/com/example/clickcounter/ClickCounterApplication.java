fd5c0c968829c49033a88be28c62ca6e3daae627
Pavel Mekhnin <mekhninpv@gmail.com>
2021-06-18 14:57:31 +0300

    Create project structure

---
+++ src/main/java/com/example/clickcounter/ClickCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.clickcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickCounterApplication.class, args);
+    }
+
+}

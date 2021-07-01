0ffdf58c25595cecd99e4ec1d24998273239f59e
Pavel Mekhnin <mekhninpv@gmail.com>
2021-07-02 01:13:51 +0500

    Optimize performance

--- src/main/java/com/example/clickcounter/service/CountServiceImpl.java
+++ src/main/java/com/example/clickcounter/service/CountServiceImpl.java
@@ -2,12 +2,26 @@ package com.example.clickcounter.service;
 
 import com.example.clickcounter.entity.Count;
 import com.example.clickcounter.repository.CountRepository;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class CountServiceImpl implements CountService {
 
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final Callable<Long> increment = new Callable<>() {
+        @Override
+        public Long call() {
+            Count current = get();
+            current.setValue(current.getValue() + 1);
+            return countRepository.save(current).getValue();
+        }
+    };
     private final CountRepository countRepository;
 
     @Autowired
@@ -16,15 +30,13 @@ public class CountServiceImpl implements CountService {
     }
 
     @Override
-    public synchronized long add() {
-        Count current = get();
-        current.setValue(current.getValue() + 1);
-        return countRepository.save(current).getValue();
+    @SneakyThrows
+    public long add()  {
+        return executorService.submit(increment).get();
     }
 
     @Override
     public Count get() {
         return countRepository.findById(1L).orElseThrow(RuntimeException::new);
     }
-
 }
